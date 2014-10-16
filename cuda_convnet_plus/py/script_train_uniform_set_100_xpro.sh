62d5cf5f4c7953907b55f65470fec0d48d45f95c
Zhicheng Yan <zyan3@illinois.edu>
2014-10-15 20:09:56 -0500

    test commit after moving files into dropbox folder

--- cuda_convnet_plus/py/script_train_uniform_set_100_xpro.sh
+++ cuda_convnet_plus/py/script_train_uniform_set_100_xpro.sh
@@ -7,4 +7,4 @@ trainData=uniform_set_100_7K_10_batch_seg_voting_k0.10
 layerDef=${layerDir}/layers-mitfivek-imgglobal-pixlocalcontext-Lab-fc192.cfg
 layerParams=${layerDir}/layer-params-mitfivek-imgglobal-pixlocalcontext-Lab-fc192.cfg
 
-python convnet.py --data-path=${dataDir}/${trainData} --save-path=${dataDir}/convnet_checkpoints --train-range=1-10 --test-range=8-10  --layer-def=${layerDef} --layer-params=${layerParams} --data-provider=mitfivek_4  --test-freq=100 --test-one=1 --gpu=0 --layer-verbose=0 --regress-L-channel-only=0 --use-local-context-ftr=1 --use-local-context-color-ftr=0 --use-position-ftr=0 --mini=512 --epochs=5000
+python convnet.py --data-path=${dataDir}/${trainData} --save-path=${dataDir}/convnet_checkpoints --train-range=1-10 --test-range=8-10  --layer-def=${layerDef} --layer-params=${layerParams} --data-provider=mitfivek_4  --test-freq=1000 --test-one=1 --gpu=0 --layer-verbose=0 --regress-L-channel-only=0 --use-local-context-ftr=1 --use-local-context-color-ftr=0 --use-position-ftr=0 --mini=512 --epochs=5000
